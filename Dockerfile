d10133b1e401040dcb6942e39eccec4f29a574d9
Daniele Rossi <daniele.rossi18@studio.unibo.it>
2024-05-30 18:26:23 +0200

    refactor: single Dockerfile. properties. Getters and setters for User model.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 ####
 # This Dockerfile is used in order to build a container that runs the Quarkus application in native (no JVM) mode.
+# It uses a micro base image, tuned for Quarkus native executables.
+# It reduces the size of the resulting container image.
+# Check https://quarkus.io/guides/quarkus-runtime-base-image for further information about this image.
 #
 # Before building the container image run:
 #
@@ -7,15 +10,14 @@
 #
 # Then, build the image with:
 #
-# docker build -f src/main/docker/Dockerfile.native -t quarkus/user-service .
+# docker build -f src/main/docker/Dockerfile -t quarkus/user-service .
 #
 # Then run the container using:
 #
 # docker run -i --rm -p 8080:8080 quarkus/user-service
 #
 ###
-FROM docker.io/redhat/ubi8-minimal:8.10-896
-
+FROM quay.io/quarkus/quarkus-micro-image:2.0
 WORKDIR /work/
 RUN chown 1001 /work \
     && chmod "g+rwX" /work \
--- src/main/java/org/acme/User/Service/User.java
+++ src/main/java/org/acme/User/Service/User.java
@@ -29,4 +29,52 @@ public class User extends PanacheEntity {
 
 	@Column(name = "user_role")
 	private String userRole;
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
 }
